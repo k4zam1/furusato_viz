b588f581a8b18485dee651b980e6d3f80e3c9e00
k4zam1 <56730772+k4zam1@users.noreply.github.com>
2020-09-15 21:51:14 +0900

    add gifts spider

--- data/satofuru/satofuru/items.py
+++ data/satofuru/satofuru/items.py
@@ -8,4 +8,14 @@ import scrapy
 class City(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    city = scrapy.Field()
+
+class Gift(scrapy.Item):
+    city = scrapy.Field()
+    link = scrapy.Field()
     name = scrapy.Field()
+    desc = scrapy.Field()
+    price = scrapy.Field()
+    star = scrapy.Field()
+    review = scrapy.Field()
+    img = scrapy.Field()
